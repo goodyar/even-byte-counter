fffa52bd5f4161839b1a63208789352332d1a3e4
Ярослав Серый <goodyar@yandex.ru>
2018-09-02 14:53:20 +0300

    Очистка кода.

--- src/main/java/local/ys/prototype/counter/EvenByteCounter.java
+++ src/main/java/local/ys/prototype/counter/EvenByteCounter.java
@@ -5,14 +5,14 @@ class EvenByteCounter {
     public static void main(String[] args) throws Exception {
         int coreNumber = Runtime.getRuntime().availableProcessors();
         int bufferSize = 1024 * 1024;
-        String pathname = "c:/test/4g.test";
+        String pathname = "";
 
-//        WorkerStarter starter = new WorkerStarter(new IOFileFactory());
-//        starter.run(1, bufferSize, pathname);
-//        starter.run(coreNumber, bufferSize, pathname);
+        WorkerStarter starter = new WorkerStarter(new IOFileFactory());
+        starter.run(1, bufferSize, pathname);
+        starter.run(coreNumber, bufferSize, pathname);
 
-        WorkerStarter starter = new WorkerStarter(new NIOFileFactory());
-//        starter.run(1, bufferSize, pathname);
+        starter = new WorkerStarter(new NIOFileFactory());
+        starter.run(1, bufferSize, pathname);
         starter.run(coreNumber, bufferSize, pathname);
     }
 }
